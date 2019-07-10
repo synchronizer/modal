0e5ca681c107628b616b121dd90ac1c977249d9e
Roman Dmitriev <roma-d@yandex-team.ru>
2019-07-10 21:04:12 +0300

    Updates

---
+++ precache-manifest.70897f2a5d2a6155871ec97a30378817.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a66c9e1cba4400b762dc88537f685d64",
+    "url": "/roma-d/modal/index.html"
+  },
+  {
+    "revision": "efc8e307df020f1cb320",
+    "url": "/roma-d/modal/static/css/main.9984f07b.chunk.css"
+  },
+  {
+    "revision": "dac96280cb35183727b3",
+    "url": "/roma-d/modal/static/js/2.020659fe.chunk.js"
+  },
+  {
+    "revision": "efc8e307df020f1cb320",
+    "url": "/roma-d/modal/static/js/main.80466616.chunk.js"
+  },
+  {
+    "revision": "58545f8f1f94d991a666",
+    "url": "/roma-d/modal/static/js/runtime~main.2bb1fa6c.js"
+  },
+  {
+    "revision": "1d3fa4cddb863d2d6128747b7a56c0d4",
+    "url": "/roma-d/modal/static/media/Screen Shot 2019-07-02 at 9.07.01 PM.1d3fa4cd.png"
+  },
+  {
+    "revision": "c71b252ba182eba0a668f9a5964bfec1",
+    "url": "/roma-d/modal/static/media/Yandeks.Karty__vybiraite_gde_poest_kuda_shodit_chem_zanyatsya_2019-06-28_15-22-21.c71b252b.png"
+  },
+  {
+    "revision": "c0d90b8f2e8a24f651541d3f4592c92b",
+    "url": "/roma-d/modal/static/media/beru.ru_product_yandex-umnaia-rozetka-yandexa-chernyi_447723001.c0d90b8f.png"
+  },
+  {
+    "revision": "972555300a8d043952dd886adc30c0f9",
+    "url": "/roma-d/modal/static/media/close.97255530.svg"
+  },
+  {
+    "revision": "0599c70e57d0f336b1e01fbd3c1cbe77",
+    "url": "/roma-d/modal/static/media/dark-scrollbar-icon.0599c70e.svg"
+  },
+  {
+    "revision": "0d5523a45244ecd00b4698687b8af6bb",
+    "url": "/roma-d/modal/static/media/default.0d5523a4.svg"
+  },
+  {
+    "revision": "9c608ec6a8083dee3bb3e181dbaf0cba",
+    "url": "/roma-d/modal/static/media/fill-vertical.9c608ec6.svg"
+  },
+  {
+    "revision": "2bce9b567196fb9476dd409fe63cb4ac",
+    "url": "/roma-d/modal/static/media/fill.2bce9b56.svg"
+  },
+  {
+    "revision": "ee14221b0544eb6d5b417540b4f7470e",
+    "url": "/roma-d/modal/static/media/light-scrollbar-icon.ee14221b.svg"
+  },
+  {
+    "revision": "6c6048166b45d051c7e065bdeba8c573",
+    "url": "/roma-d/modal/static/media/scroll.6c604816.svg"
+  }
+]);
